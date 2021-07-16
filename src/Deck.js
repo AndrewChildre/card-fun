354316cda3f92048f507786f11fe00d720699cec
andrew childre <andrewchildre@gmail.com>
2021-07-15 20:47:08 -0600

    api calls work

---
+++ src/Deck.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+
+
+const Deck = () => {
+    const [cardNum, setCardNum] = useState('');
+    const [cards, setCards] = useState(null);
+
+    const cardData = (e) => {
+        e.preventDefault();
+        fetch(
+					`https://deckofcardsapi.com/api/deck/new/draw/?count=${cardNum}`
+				)
+                .then(res => res.json())
+                .then(data => {setCards(data)
+                console.log(data);});
+               
+
+    }
+    const handleChange = (e) => {
+        e.preventDefault();
+        setCardNum(e.target.value)
+    }
+
+    return (
+        <div>
+            <form name='cardNum' onChange={(e) => {handleChange(e)}}>
+                <input type="text" />
+                <button onClick={cardData}>X</button>
+            </form>
+            <div className='card-display'>
+
+                {cards && cards.cards.map((card, index) => {
+                    return (
+                        <div key={index}>
+                            <img src={card.image} alt="card" srcset="" />
+                            <h2>{cards.remaining}</h2>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Deck;
