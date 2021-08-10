e7fb244152340e44f19fa19686a3b7115392a662
andrew childre <andrewchildre@gmail.com>
2021-08-10 12:24:20 -0600

    update top

--- src/Deck.js
+++ src/Deck.js
@@ -1,57 +1,57 @@
-// import React, { useState } from 'react';
-// import Nav from './Nav';
+import React, { useState } from 'react';
+import Nav from './Nav';
 
-// const Deck = () => {
-// 	const [cardNum, setCardNum] = useState('');
-// 	const [cards, setCards] = useState(null);
-//     	const handleChange = (e) => {
-// 		// e.preventDefault();
+const Deck = () => {
+	const [cardNum, setCardNum] = useState('');
+	const [cards, setCards] = useState(null);
+    	const handleChange = (e) => {
+		// e.preventDefault();
      
     
-// 	};
+	};
 
-// 	const cardData = (e) => {
-// 		e.preventDefault();
-// 		fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${cardNum}`)
-// 			.then((res) => res.json())
-// 			.then((data) => {
-// 				setCards(data);
-// 			});
-// 	};
+	const cardData = (e) => {
+		e.preventDefault();
+		fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${cardNum}`)
+			.then((res) => res.json())
+			.then((data) => {
+				setCards(data);
+			});
+	};
 
    
-// 	return (
-// 		<div>
+	return (
+		<div>
             
-//             <div className = 'form-div'>
-//                 <h1>Card Shuffle</h1>
-// 			<form
-// 				className='form-top'
-// 				name='cardNum'
-//                 onChange={(e) => {
-// 			setCardNum(e.target.value);
+            <div className = 'form-div'>
+                <h1>Card Shuffle</h1>
+			<form
+				className='form-top'
+				name='cardNum'
+                onChange={(e) => {
+			setCardNum(e.target.value);
 
-// 				}}
-// 				>
-// 				<input id='text-box' type='text' placeholder='How Many' />
-// 				<button onClick={cardData}>Deal</button>
+				}}
+				>
+				<input id='text-box' type='text' placeholder='How Many' />
+				<button onClick={cardData}>Deal</button>
                 
-// 			</form>
+			</form>
            
             
-//                     </div>
-// 			<div className='card-display'>
-// 				{cards &&
-// 					cards.cards.map((card, index) => {
-// 						return (
-// 							<div key={index}>
-// 								<img src={card.image} alt='card' srcset='' />
-// 							</div>
-// 						);
-// 					})}
-// 			</div>
-// 		</div>
-// 	);
-// };
+                    </div>
+			<div className='card-display'>
+				{cards &&
+					cards.cards.map((card, index) => {
+						return (
+							<div key={index}>
+								<img src={card.image} alt='card' srcset='' />
+							</div>
+						);
+					})}
+			</div>
+		</div>
+	);
+};
 
-// export default Deck;
+export default Deck;
